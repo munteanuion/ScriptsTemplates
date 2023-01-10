1b4e5b75a193da1311acb67751dfc3f0c9c69ff5
munteanuion <71140604+munteanuion@users.noreply.github.com>
2023-01-11 00:20:19 +0200

    change camera urmareste

--- CameraUrmaresteObiectul.cs
+++ CameraUrmaresteObiectul.cs
@@ -2,15 +2,77 @@ using UnityEngine;
 
 public class CameraUrmaresteObiectul : MonoBehaviour
 {
+    enum AxaDeUrmarire
+    {
+        X,
+        Y,
+        Z,
+        XY,
+        XZ,
+        YZ,
+        XYZ
+    }
+
     [SerializeField] private Transform _peCineUrmareste;
     [SerializeField] private Vector3 _pozitiaFataDeUrmaritor;
     [SerializeField] private float _vitezaDeUrmarire = 0.1f;
-    
+    [SerializeField] private AxaDeUrmarire _axaDeUrmarire;
+
+    private Vector3 _initialPosition;
+
+    private void Start()
+    {
+        _initialPosition = _peCineUrmareste.transform.position;
+    }
+
     private void Update()
     {
-        transform.position = Vector3.Lerp(
-            transform.position, 
-            new Vector3(_peCineUrmareste.position.x + _pozitiaFataDeUrmaritor.x, _peCineUrmareste.position.y, _peCineUrmareste.position.z) + _pozitiaFataDeUrmaritor, 
-            _vitezaDeUrmarire);
+        switch (_axaDeUrmarire)
+        {
+            case AxaDeUrmarire.X:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_peCineUrmareste.position.x, _initialPosition.y, _initialPosition.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            case AxaDeUrmarire.Y:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_initialPosition.x, _peCineUrmareste.position.y, _initialPosition.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            case AxaDeUrmarire.Z:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_initialPosition.x, _initialPosition.y, _peCineUrmareste.position.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            case AxaDeUrmarire.XY:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_peCineUrmareste.position.x, _peCineUrmareste.position.y, _initialPosition.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            case AxaDeUrmarire.XZ:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_peCineUrmareste.position.x, _initialPosition.y, _peCineUrmareste.position.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            case AxaDeUrmarire.YZ:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_initialPosition.x, _peCineUrmareste.position.y, _peCineUrmareste.position.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            case AxaDeUrmarire.XYZ:
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    new Vector3(_peCineUrmareste.position.x, _peCineUrmareste.position.y, _peCineUrmareste.position.z) + _pozitiaFataDeUrmaritor,
+                    _vitezaDeUrmarire);
+                break;
+            default:
+                break;
+        }
     }
 }
